b0873b065a1af69df14ff99f94145036c992b364
Anmir12 <skannu30@gmail.com>
2026-06-24 12:47:57 +0530

    http methods test

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
 const PORT = 7777;
 
-app.get("/",(req,res)=>{
- res.send("hello from homepage")
-})
+app.get("/user", (req, res) => {
+  res.send("hello from homepage");
+});
+app.post("/user", (req, res) => {
+  res.send({
+    name:"anmir",
+    id:786
+  });
+  console.log("user add succesfully")
+});
+app.put("/user", (req, res) => {
+  res.send("user updated succesfully");
+  console.log("user updated succesfully")
 
-app.get("/hello",(req,res)=>{
-  res.send("hello hello hello")
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("user deleted succesfully")
+  console.log("user deleted succesfully")
+
 })
-app.listen(PORT,()=>{
-    console.log(`listening at port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}`);
 });

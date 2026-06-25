790a9e6b87ad3243e52e2eaad105e54ec52b9502
Anmir12 <skannu30@gmail.com>
2026-06-25 11:41:20 +0530

    playing around req handler params

--- src/app.js
+++ src/app.js
@@ -4,31 +4,19 @@ const app = express();
 
 const PORT = 7777;
 
-app.use("/",(req,res)=>{
-  res.send("use is middleware and order matters a lot so it will execute first and bypass all the requests")
-})
+app.use(
+  "/user",
+  (req, res, next) => {
+    res.send("request handler 1");
+    console.log("req handler 1 executed");
+    next();
+  },
+  (req, res) => {
+    res.send("request handler 2");
+    console.log("request handler 2 executed");
+  },
+);
 
-app.get("/user", (req, res) => {
-  res.send("hello from homepage");
-});
-app.post("/user", (req, res) => {
-  res.send({
-    name:"anmir",
-    id:786
-  });
-  console.log("user add succesfully")
-});
-app.put("/user", (req, res) => {
-  res.send("user updated succesfully");
-  console.log("user updated succesfully")
-
-});
-
-app.delete("/user",(req,res)=>{
-  res.send("user deleted succesfully")
-  console.log("user deleted succesfully")
-
-})
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
 });

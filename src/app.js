66dbaf0c2e8959102caefa360d36b07e80d5092b
Anmir12 <skannu30@gmail.com>
2026-06-27 07:05:25 +0530

    exploring route handlers with next param

--- src/app.js
+++ src/app.js
@@ -4,24 +4,18 @@ const app = express();
 
 const PORT = 7777;
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    // res.send("request handler 1");
-    console.log("req handler 1 executed");
-    console.log(req.query)
-    next();
-  },
-  (req, res,next) => {
-    // res.send("request handler 2");
-    next()
-    console.log("request handler 2 executed");
-  },
-  (req, res) => {
-    res.send("request handler 3");
-    console.log("request handler 3 executed");
-  },
-);
+app.use("/user", (req, res, next) => {
+  // res.send("request handler 1");
+  console.log("req handler 1 executed");
+  console.log(req.query);
+  next();
+});
+
+app.use("/user",(req,res,next)=>{
+  res.send("req handler 2 is executed");
+  console.log("req handler 2");
+  next();
+})
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);

e38605c71fedd8ada226b096716a14b6329ea5b2
Anmir12 <skannu30@gmail.com>
2026-06-27 08:11:39 +0530

    writing a utility middlewear function for /admin route

--- src/app.js
+++ src/app.js
@@ -4,41 +4,25 @@ const app = express();
 
 const PORT = 7777;
 
-// app.use("/admin",(req,res,next)=>{
-
-// })
-
-app.get("/admin/getAllData",(req,res,next)=>{
-   
+app.use("/admin", (req, res, next) => {
   const token = "xyz";
-  
-  const isAdminAuthorized = token === "xyz"
-  console.log("checking admin authorization")
-  if(isAdminAuthorized){
-    res.send("sending all admin data")
+
+  const isAdminAuthorized = token === "xyz";
+
+  if (!isAdminAuthorized) {
+    res.status(401).send("admin authentication failed");
+  } else {
+    next();
   }
+});
 
-else{
-  res.status(401).send("authorization failed");
-}
-})
+app.get("/admin/getAllData", (req, res, next) => {
+  res.send("sending all admin data");
+});
 
-
-app.delete("/admin/deleteAllData",(req,res,next)=>{
- 
-    const token = "xyz";
-  
-  const isAdminAuthorized = token === "xyz"
-  console.log("checking admin authorization")
-  if(isAdminAuthorized){
-    res.send("deleting all admin data")
-  }
-
-else{
-  res.status(401).send("authorization failed");
-}
-
-})
+app.delete("/admin/deleteAllData", (req, res, next) => {
+  res.send("deleting all admin data");
+});
 
 
 

ac03ba96c85df1f857c2f7f9ae6d89c2663f6daf
Anmir12 <skannu30@gmail.com>
2026-06-27 08:06:00 +0530

    writing dummy auth middleware

--- src/app.js
+++ src/app.js
@@ -4,18 +4,43 @@ const app = express();
 
 const PORT = 7777;
 
-app.use("/user", (req, res, next) => {
-  // res.send("request handler 1");
-  console.log("req handler 1 executed");
-  console.log(req.query);
-  next();
-});
+// app.use("/admin",(req,res,next)=>{
 
-app.use("/user",(req,res,next)=>{
-  res.send("req handler 2 is executed");
-  console.log("req handler 2");
-  next();
+// })
+
+app.get("/admin/getAllData",(req,res,next)=>{
+   
+  const token = "xyz";
+  
+  const isAdminAuthorized = token === "xyz"
+  console.log("checking admin authorization")
+  if(isAdminAuthorized){
+    res.send("sending all admin data")
+  }
+
+else{
+  res.status(401).send("authorization failed");
+}
 })
+
+
+app.delete("/admin/deleteAllData",(req,res,next)=>{
+ 
+    const token = "xyz";
+  
+  const isAdminAuthorized = token === "xyz"
+  console.log("checking admin authorization")
+  if(isAdminAuthorized){
+    res.send("deleting all admin data")
+  }
+
+else{
+  res.status(401).send("authorization failed");
+}
+
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
